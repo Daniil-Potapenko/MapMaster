34770f1e57a826761ca9123f4882ef538746df6f
daniil <daniil.potapenko@gmail.com>
2023-12-08 11:21:28 +0300

    delete account module/ some work on user module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { User } from 'src/account/schemas/user.schema';
+import { Model, ObjectId } from 'mongoose';
+import { User, UserDocument } from 'src/user/schema/user.schema';
+import { userEntity } from './entity/user.entity';
+import { createUserDto } from './entity/user.dto';
+import { UserModule } from './user.module';
+import { IUser } from './entity/user.interface';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>){}
 
-  createUser(){
+  async createUser({email,firstName,lastName,password}: createUserDto): Promise<User> {
+    const newUserEntity = new userEntity({
+      email,
+      firstName,
+      lastName
+    }).setPassword(password)
+
+  const createdUser = await new this.userModel(userEntity).save()
+
+  return createdUser.id
 
   }
 
@@ -16,6 +29,15 @@ export class UserService {
   }
 
   updateUser(){
+
+  }
+
+  async findUser(email:string):Promise<User|false>{
+    const user = await this.userModel.findOne({email})
+    if(user){
+      return user
+    }
+    else return false
 
   }
   
